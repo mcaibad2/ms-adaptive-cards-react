d0a6c719fc911cdcb92c458d45094fba266c958c
Andreas Daskalopoulos <mcaibad2@gmail.com>
2019-05-22 12:38:24 +0300

    Added my first card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AdaptiveCard from 'react-adaptivecards'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// see http://adaptivecards.io/samples/ for inspiration
+var card = {
+    "type": "AdaptiveCard",
+    "version": "1.0",
+    "body": [{
+        "type": "TextBlock",
+        "text": "Here is a ninja cat"
+    }, {
+        "type": "Image",
+        "url": "http://adaptivecards.io/content/cats/1.png"
+    }]
+};
+
+const app = () => (
+    <div style={{ margin: '50px 0', padding: '0 50px' }}>
+        <h2>React - Adaptive Cards Example</h2>
+        <AdaptiveCard payload={card} />
     </div>
-  );
-}
+)
 
-export default App;
+export default app;
