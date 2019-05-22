79efe6d893b0cfd82644d89c3d737137cabc1820
Andreas Daskalopoulos <mcaibad2@gmail.com>
2019-05-22 12:46:39 +0300

    Style refactoring

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ var card = {
 const app = () => (
     <div style={{ margin: '50px 0', padding: '0 50px' }}>
         <h2>React - Adaptive Cards Example</h2>
-        <AdaptiveCard payload={card} />
+        <AdaptiveCard payload={card} style={{ border: '1px solid black' }} />
     </div>
 )
 
